6750c4b30149ab1f90c9e11e2adae05f61ea001c
abharadwaj310 <alamuri0310@gmail.com>
2024-04-05 23:20:50 -0400

    Update streamlit_app.py
    adding the REST API call to the end of the code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-#New section to display fruityvice nutrition information
-import requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie  :cup_with_straw:")
 st.write(
@@ -46,3 +40,8 @@ if ingredients_list:
     if time_to_submit:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!')
+
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
