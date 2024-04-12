108a7992656a24adb41a027ed32ff14279cdc98c
abharadwaj310 <alamuri0310@gmail.com>
2024-04-12 11:07:48 -0400

    Update streamlit_app.py
    Adding nutrition information to the each fruit chosen.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ if ingredients_list:
 
     for each_fruit_chosen in ingredients_list:
         ingredients_string += each_fruit_chosen + ' ' 
+        st.subheader(each_fruit_chosen + 'Nutrition Information')
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', each_fruit_chosen,' is ', search_on, '.')
         
